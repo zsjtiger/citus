5a0ed7edb3bad38169089df2589a6c8e51311e25
Nils Dijk <nils@citusdata.com>
2020-12-16 13:42:29 +0100

    make ci pass on our build script

--- ci/build-extension.sh
+++ ci/build-extension.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # make bash behave
-set -euxo pipefail
+set -euo pipefail
 IFS=$'\n\t'
+
+# shellcheck disable=SC1091
+source ci/ci_helpers.sh
 
 # read pg major version, error if not provided
 PG_MAJOR=${PG_MAJOR:?please provide the postgres major version}
