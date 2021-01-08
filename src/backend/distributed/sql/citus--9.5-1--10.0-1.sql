d0d87973bddb4d44966cc80385642a57a1aa46f4
Nils Dijk <nils@citusdata.com>
2021-01-08 13:09:08 +0100

    expand shard model for geo distribution

--- src/backend/distributed/sql/citus--9.5-1--10.0-1.sql
+++ src/backend/distributed/sql/citus--9.5-1--10.0-1.sql
@@ -3,6 +3,9 @@
 DROP FUNCTION pg_catalog.upgrade_to_reference_table(regclass);
 DROP FUNCTION IF EXISTS pg_catalog.citus_total_relation_size(regclass);
 
+-- add a catchall shardcontainer - eg. geobbox
+ALTER TABLE pg_catalog.pg_dist_shard ADD COLUMN shardcontainer text;
+
 #include "udfs/citus_total_relation_size/10.0-1.sql"
 #include "udfs/citus_tables/10.0-1.sql"
 #include "udfs/citus_finish_pg_upgrade/10.0-1.sql"
