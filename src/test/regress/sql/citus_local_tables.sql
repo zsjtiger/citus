fdb85dc5cd1b832a6be16b15d2580cd4ba4d980e
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-08-25 15:46:21 +0300

    Style

--- src/test/regress/sql/citus_local_tables.sql
+++ src/test/regress/sql/citus_local_tables.sql
@@ -556,13 +556,13 @@ ORDER BY child;
 
 -- verify we successfully create foreign keys
 SELECT
-    tc.table_schema, 
-    tc.constraint_name, 
-    tc.table_name, 
+    tc.table_schema,
+    tc.constraint_name,
+    tc.table_name,
     ccu.table_schema AS foreign_table_schema,
     ccu.table_name AS foreign_table_name
-FROM 
-    information_schema.table_constraints AS tc 
+FROM
+    information_schema.table_constraints AS tc
     JOIN information_schema.key_column_usage AS kcu
       ON tc.constraint_name = kcu.constraint_name
       AND tc.table_schema = kcu.table_schema
@@ -599,13 +599,13 @@ ORDER BY child;
 
 -- verify we successfully create foreign keys
 SELECT
-    tc.table_schema, 
-    tc.constraint_name, 
-    tc.table_name, 
+    tc.table_schema,
+    tc.constraint_name,
+    tc.table_name,
     ccu.table_schema AS foreign_table_schema,
     ccu.table_name AS foreign_table_name
-FROM 
-    information_schema.table_constraints AS tc 
+FROM
+    information_schema.table_constraints AS tc
     JOIN information_schema.key_column_usage AS kcu
       ON tc.constraint_name = kcu.constraint_name
       AND tc.table_schema = kcu.table_schema
