e63bdfc49f9203db14ef77313c1d5e3461a84a32
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-08-26 13:25:23 +0300

    Add option PROCESS_TOAST to VACUUM - PG14 #7cb3048

--- src/test/regress/sql/pg14.sql
+++ src/test/regress/sql/pg14.sql
@@ -5,3 +5,19 @@ SELECT substring(:'server_version', '\d+')::int > 13 AS server_version_above_thi
 \else
 \q
 \endif
+CREATE SCHEMA pg14test;
+SET search_path TO "pg14test";
+SET citus.next_shard_id TO 980000;
+SET citus.shard_count TO 2;
+
+-- test the new vacuum option, process_toast
+CREATE TABLE t1 (a int);
+SELECT create_distributed_table('t1','a');
+SET citus.log_remote_commands TO ON;
+VACUUM (FULL) t1;
+VACUUM (FULL, PROCESS_TOAST) t1;
+VACUUM (FULL, PROCESS_TOAST true) t1;
+VACUUM (FULL, PROCESS_TOAST false) t1;
+
+SET client_min_messages TO WARNING;
+DROP SCHEMA pg14test CASCADE;
